df29e6678efababfdc9be597f32f2f32b580308d
mo1501 <mr.mo1501abdullahi@gmail.com>
2022-09-24 14:56:07 +0300

    Added Homescreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:circular_menu/circular_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,36 +10,104 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Homescreen'),
-        actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-            items: [
-              DropdownMenuItem(
-                child: Container(
-                  child: Row(children: [
-                    Icon(Icons.exit_to_app),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text('Logout'),
-                  ]),
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Homescreen'),
+          actions: [
+            DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout'),
+                    ]),
+                  ),
+                  value: 'Logout',
                 ),
-                value: 'Logout',
+              ],
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'Logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: CircularMenuItem(
+                          iconSize: 30.0,
+                          icon: Icons.person,
+                          color: Theme.of(context).primaryColor,
+                          onTap: () {}),
+                    ),
+                  ],
+                ),
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: CircularMenuItem(
+                        icon: Icons.camera_alt_rounded,
+                        color: Theme.of(context).primaryColor,
+                        onTap: () {},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: CircularMenuItem(
+                        icon: Icons.photo_album_sharp,
+                        color: Theme.of(context).primaryColor,
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: CircularMenuItem(
+                        icon: Icons.map_outlined,
+                        color: Theme.of(context).primaryColor,
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
-            onChanged: (itemIdentifier) {
-              if (itemIdentifier == 'Logout') {
-                FirebaseAuth.instance.signOut();
-              }
-            },
           ),
-        ],
-      ),
-    );
+        ),
+        );
   }
 }
